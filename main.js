d98b9570492d90cc2927df59e38cca1e04833473
Jason E Plumb <jasoneplumb@gmail.com>
2023-10-21 20:35:38 -0700

    corrected license

--- main.js
+++ main.js
@@ -1,4 +1,22 @@
-// main.js
+/***********************************************************
+Licensed to the Apache Software Foundation (ASF) under one
+or more contributor license agreements.  See the NOTICE file
+distributed with this work for additional information
+regarding copyright ownership.  The ASF licenses this file
+to you under the Apache License, Version 2.0 (the
+"License"); you may not use this file except in compliance
+with the License.  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing,
+software distributed under the License is distributed on an
+"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, either express or implied.  See the License for the
+specific language governing permissions and limitations
+under the License.
+***********************************************************/
+// File: ./main.js
 // https://www.electronjs.org/docs/latest/tutorial/process-model
 /* jshint esversion:8, node:true, -W033, -W014 */// 033 Missing semicolon, 014 Permissive line breaks
 'use strict'
